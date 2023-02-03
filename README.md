3095bde8c792b15b04063ca9967a8b2011d32e45
agladman <20296658+agladman@users.noreply.github.com>
2023-02-03 11:13:59 +0000

    moving amazon.py into project

--- README.md
+++ README.md
@@ -20,3 +20,13 @@ Have Hazel move a Virgin Media bill into the `_virgin` folder and run `virgin.py
 {% warning %}
 **Warning:** remember to pass the PDF filename in quotes when calling this script from the terminal, e.g. `python3 virgin.py "_virgin/Virgin Media bill_2023_01_16.pdf"`
 {% endwarning %}
+
+## Amazon Orders
+Save order confirmation emails as text files, have Hazel move them to the `_Amazon` folder and run `amazon.py`.
+The script will:
+
+- Extract the order date
+- Extract the order number
+- Extract the order amount
+- Extract the item ordered
+- Write this information to a file in Obsidian
---
+++ amazon.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+"""Extracts order info from Amazon order confirmation emails exported to plain text
+run with -t to print results to terminal rather than outputting to file
+"""
+
+import datetime as dt
+from pathlib import Path
+import re
+import sys
+
+
+def extract_data(text, target=None):
+    "searches the text for the designated target data"
+    match target:
+        case "order_number":
+            pattern = re.compile("Order #(.+)")
+        case "order_date":
+            pattern = re.compile("Date: (.+)")
+        case "order_total":
+            pattern = re.compile("[Order|Order Grand] Total:\s?\n(.+)")
+        case "order_item":
+            pattern = re.compile("Subject:.+order of (.+)")
+        case _:
+            raise ValueError
+    result = re.search(pattern, text)
+    if result:
+        return result.group(1).strip()
+    else:
+        raise EOFError
+
+def clean_date(date_string):
+    "converts date string into formatted datetime object"
+    date_obj = dt.datetime.strptime(date_string, "%d %B %Y at %H:%M:%S %Z").date()
+    return date_obj.strftime("%d %b %Y")
+
+def main():
+    file_in = Path(sys.argv[1])
+    text = file_in.read_text()
+    onumber = extract_data(text, target="order_number")
+    odate = extract_data(text, target="order_date")
+    odate = clean_date(odate)
+    ototal = extract_data(text, target="order_total")
+    oitem = extract_data(text, target="order_item")
+    output = f"- [ ] {odate}; total {ototal}; order {onumber}: {oitem}\n"
+    if "-t" in sys.argv:
+        print(output)
+    else:
+        file_out = Path("/Users/anthonygladman/Library/Mobile Documents/iCloud~md~obsidian/Documents/BibScrib/Household stuff/Amazon Purchases for YNAB.md")
+        with file_out.open("a") as f:
+            f.write(output)
+        file_in.unlink()
+
+if __name__ == '__main__':
+    main()
